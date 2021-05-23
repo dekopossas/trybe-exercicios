fca56649dee0f9dbfb8222cf396f22b94d112871
deko possas <andrepossaster@gmail.com>
2021-05-23 15:43:05 -0300

    fizzbuzz funcionando e testado com black

--- exercises/python/fizzbuzz.py
+++ exercises/python/fizzbuzz.py
@@ -1,10 +1,18 @@
 """Fizz buzz com python"""
 
+
 def fizzbuzz(numero):
     """Recebe um numero, retorna fizz, buzz, fizzbuzz ou o numero"""
-    return 'fizz'
+    if (numero % 3 == 0) and (numero % 5 == 0):
+        return "fizzbuzz"
+    if numero % 5 == 0:
+        return "buzz"
+    if numero % 3 == 0:
+        return "fizz"
+    return str(numero)
 
-assert fizzbuzz(3) == 'fizz'
-assert fizzbuzz(5) == 'buzz'
-assert fizzbuzz(15) == 'fizzbuzz'
-assert fizzbuzz(4) == '4'
+
+assert fizzbuzz(3) == "fizz"
+assert fizzbuzz(5) == "buzz"
+assert fizzbuzz(15) == "fizzbuzz"
+assert fizzbuzz(4) == "4"
