700c2be2e8a59b9f647e476c69860835619c7661
deko possas <andrepossaster@gmail.com>
2021-05-22 23:52:57 -0300

    pokemons de grama

--- exercises/python/pokemon3.py
+++ exercises/python/pokemon3.py
@@ -3,10 +3,11 @@ import json
 with open("pokemons.json") as file:
     pokemons = json.load(file)["results"]
 
-grass_type_pokemon = [
-  pokemon for pokemon in pokemons if "grass" in pokemon["type"]
+grass_type_pokemons = [
+    pokemon for pokemon in pokemons if "Grass" in pokemon["type"]
 ]
 
 with open("grass_pokemons.json", "w") as file:
-    json_to_write = json.dump(grass_type_pokemon)
+    json_to_write = json.dumps(grass_type_pokemons)
     file.write(json_to_write)
+    
