04cd673c6040eb4a8241d442041f57757efb37c2
deko possas <andrepossaster@gmail.com>
2021-05-23 23:43:50 -0300

    key board interrupting

--- exercises/python/fizzplz.py
+++ exercises/python/fizzplz.py
@@ -1,10 +1,15 @@
 """Pede um número para o usuário"""
 from fizzbuzz import fizzbuzz
+import sys
 
 
 def pedir_numero():
     """Pede numero e retorna fizzbuzz"""
-    resposta = input("Digite um número inteiro por favor: ")
+    try:
+        resposta = input("Digite um número inteiro por favor: ")
+    except KeyboardInterrupt:
+        print("\n deixa quieto")
+
     try:
         return int(resposta)
     except ValueError:
