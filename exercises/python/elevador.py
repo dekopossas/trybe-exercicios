86b77e04d8e25f6d43cf72b9ce9664804af6a42e
deko possas <andrepossaster@gmail.com>
2021-05-25 20:32:52 -0300

    tudo correto agora kkkk

--- exercises/python/elevador.py
+++ exercises/python/elevador.py
@@ -1,30 +1,25 @@
 
 class Elevador:
-  """Essa classe transforma uma variável em um elevador"""
+    """Essa classe transforma uma variável em um elevador"""
     def __init__(self):
         self.andar = 0
         self.chamados = []
 
-    def chamar (andar):
+    def chamar (self, andar):
         """adiciona o andar chamado na lista de chamados"""
-        self.chamados.append(self, andar)
+        self.chamados.append(andar)
 
-elevador_1:Elevador()
-elevador_2:Elevador()
+    def ir(self):
+        """Vai para o próximo andar da lista de chamados"""
+        if self.chamados:
+            self.andar = self.chamados[0]
+            self.chamados = self.chamados[1:]
 
-def ir():
-    """Vai para o próximo andar da lista de chamados"""
-    global chamados # usando variavel de fora do escopo
-    global andar # usando variavel de fora do escopo
-    if chamados:
-        andar = chamados[0]
-        chamados = chamados[1:]
+elevador_1 = Elevador()
+elevador_2 = Elevador()
 
-chamar(2)
-chamar(3)
-chamar(0)
+elevador_1.chamar(3)
+elevador_1.chamar(4)
+elevador_1.ir()
 
-ir()
-ir()
-
-assert andar == 3
+assert elevador_1.andar == 3
