cb3706d62e9bc0a60fba1d5b2ccdb908687e7430
deko possas <andrepossaster@gmail.com>
2021-05-25 19:16:01 -0300

    elevador funcionando

---
+++ exercises/python/elevador.py
@@ -0,0 +1,10 @@
+andar = 0
+chamados = []
+
+def chamar (andar):
+    """adiciona o andar chamado na lista de chamados"""
+    chamados.append(andar)
+
+chamar(2)
+chamados.append(2)
+chamados = [2]
