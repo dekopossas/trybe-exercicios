1cd945233eecd2b6ae0a4697a63d80aec30694ab
deko possas <andrepossaster@gmail.com>
2021-05-25 20:58:55 -0300

    sugeri nao mexer nas variaveis

--- exercises/python/elevador.py
+++ exercises/python/elevador.py
@@ -2,18 +2,22 @@
 class Elevador:
     """Essa classe transforma uma variável em um elevador"""
     def __init__(self):
-        self.andar = 0
-        self.chamados = []
+        self._andar = 0
+        self._chamados = []
 
     def chamar (self, andar):
         """adiciona o andar chamado na lista de chamados"""
-        self.chamados.append(andar)
+        self._chamados.append(andar)
 
     def ir(self):
         """Vai para o próximo andar da lista de chamados"""
-        if self.chamados:
-            self.andar = self.chamados[0]
-            self.chamados = self.chamados[1:]
+        if self._chamados:
+            self._andar = self._chamados[0]
+            self._chamados = self._chamados[1:]
+
+    def localizar(self):
+        """ Indica o andar do elevador"""
+        return self._andar
 
 elevador_1 = Elevador()
 elevador_2 = Elevador()
@@ -21,5 +25,6 @@ elevador_2 = Elevador()
 elevador_1.chamar(3)
 elevador_1.chamar(4)
 elevador_1.ir()
+elevador_1.ir()
 
-assert elevador_1.andar == 3
+assert elevador_1.localizar() == 4
