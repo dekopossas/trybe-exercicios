0b26fc34f99832d5d14075af962d9062c9bacf25
deko possas <andrepossaster@gmail.com>
2021-05-25 19:21:11 -0300

    entendendo a dor de POO

--- exercises/python/elevador.py
+++ exercises/python/elevador.py
@@ -5,6 +5,16 @@ def chamar (andar):
     """adiciona o andar chamado na lista de chamados"""
     chamados.append(andar)
 
+def ir():
+    """Vai para o próximo andar da lista de chamados"""
+    if chamados:
+        andar = chamados[0]
+        chamados = chamados[1:]
+
 chamar(2)
 chamados.append(2)
 chamados = [2]
+
+andar = 3
+ir()
+andar = chamados[0]
