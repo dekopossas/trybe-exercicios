3ec3c463f0395d8eb4b5110fe09ad90de041fee9
deko possas <andrepossaster@gmail.com>
2021-05-24 22:46:06 -0300

    teste de devide

--- exercises/python/test_codigo.py
+++ exercises/python/test_codigo.py
@@ -1,6 +1,9 @@
 from codigo import is_even
-
+from codigo import divide
 
 def test_is_even_number_is_even_return_true():
     assert is_even(2) is True
-    
+
+
+def test_divide_thrown_exception_when_dividing_by_zero():
+    assert divide(2, 0) is False
