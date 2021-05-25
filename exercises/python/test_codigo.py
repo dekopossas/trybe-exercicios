049e6181a0110baaca3d9766b1a09f86705e3ac0
deko possas <andrepossaster@gmail.com>
2021-05-24 22:49:58 -0300

    teste com excessao

--- exercises/python/test_codigo.py
+++ exercises/python/test_codigo.py
@@ -1,9 +1,11 @@
 from codigo import is_even
 from codigo import divide
+import pytest
 
 def test_is_even_number_is_even_return_true():
     assert is_even(2) is True
 
 
 def test_divide_thrown_exception_when_dividing_by_zero():
-    assert divide(2, 0) is False
+    with pytest.raises(ZeroDivisionError, match='division by zero'):
+        divide(2, 0)
