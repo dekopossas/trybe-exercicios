38d6d85016b2dae71e61a8ecb2ffc51222d8d94e
deko possas <andrepossaster@gmail.com>
2021-05-25 22:34:57 -0300

    chamados underline corrigido

--- exercises/python/elevators/elevador_2.py
+++ exercises/python/elevators/elevador_2.py
@@ -3,7 +3,7 @@ from elevators.elevador import Elevador
 class TerreoElevador(Elevador):
     def ir(self):
         """Volta pro terreio se estiver sem chamadas"""
-        if not self.chamados:
+        if not self._chamados:
             self._andar = 0
         else:
             super().ir() # metodo irado de falar q ta usando function base.
