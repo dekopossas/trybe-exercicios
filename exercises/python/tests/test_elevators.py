48160ae75cf06c0106ebd2b60cce9f2eba9db506
deko possas <andrepossaster@gmail.com>
2021-05-25 22:11:09 -0300

    tdd de elevador do terreo

--- exercises/python/tests/test_elevators.py
+++ exercises/python/tests/test_elevators.py
@@ -11,3 +11,15 @@ def test_elevador():
     elevador.ir
     # assert
     assert elevador.localizar() == 2
+
+def test_elevador_2():
+    # arrange
+    elevador = TerreoElevador()
+    # act
+    elevador.chamar(2)
+    elevador.chamar(4)
+    elevador.ir()
+    elevador.ir()
+    elevador.ir()
+    assert elevador.localizar() == 0
+    
