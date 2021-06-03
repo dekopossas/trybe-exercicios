b838ea5a4435e69a56f7fcebb2866ce66f0f26d1
deko possas <andrepossaster@gmail.com>
2021-06-02 23:53:57 -0300

    um quarto

--- exercises/37_1/aula_melhorada.py
+++ exercises/37_1/aula_melhorada.py
@@ -3,21 +3,16 @@ numeros = [1, 2, 2, 6, 6, 6, 6, 7, 10]
 print(len(numeros))
 def numero_que_aparece_um_quarto(numeros):
     contador = dict()
+    quarto = len(numeros) / 4
 
     for numero in numeros:
         contador[numero] = contador.get(numero, 0) + 1
 
     print(contador)
 
-    numero_mais_frequente = None
-    maior_contagem = 0
-
     for numero, contagem in contador.items():
-        if contagem > maior_contagem:
-            numero_mais_frequente = numero
-            maior_contagem = contagem
-
-    return numero_mais_frequente
+        if contagem >= quarto:
+            return numero
 
 resultado = numero_que_aparece_um_quarto(numeros)
 
