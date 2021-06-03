21f9c77e8c0ac61146baf18700343f39dd71418a
deko possas <andrepossaster@gmail.com>
2021-06-02 22:56:35 -0300

    nova logica

--- exercises/37_1/aula.py
+++ exercises/37_1/aula.py
@@ -7,4 +7,9 @@ contador = dict()
 for numero in numeros:
     contador[numero] = contador.get(numero, 0) + 1
 
+for numero, contagem in contador.item():
+    if contagem > maior_contagem:
+        maior_numero = numero
+        maior_contagem = contagem
+
 print(contador)
